bf05b338534a62d9c93265ad3dbcdd0543d9fd41
D A <dfa2222@gmail.com>
2020-04-06 07:02:59 -0400

    Updates

---
+++ precache-manifest.8d34c5e3a06083fa82dcb985689545cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea7a6c96e687e05ece0abc299b472afd",
+    "url": "/edsocial-demo/index.html"
+  },
+  {
+    "revision": "4e141a34b3250f139de6",
+    "url": "/edsocial-demo/static/css/main.e74facf6.chunk.css"
+  },
+  {
+    "revision": "afd42a99fe0f68acdf33",
+    "url": "/edsocial-demo/static/js/2.dead9897.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/edsocial-demo/static/js/2.dead9897.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e141a34b3250f139de6",
+    "url": "/edsocial-demo/static/js/main.b029e6e3.chunk.js"
+  },
+  {
+    "revision": "e3d878c406bbf9a5e82f",
+    "url": "/edsocial-demo/static/js/runtime-main.6e0498ca.js"
+  }
+]);
